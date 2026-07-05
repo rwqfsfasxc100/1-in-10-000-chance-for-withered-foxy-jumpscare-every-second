033113f6f9a63b29129cb0e2659995fe868a6836
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-07-05 09:38:13 -0700

    Updated config fetching

--- FoxyLayer.gd
+++ FoxyLayer.gd
@@ -1,19 +1,11 @@
 extends CanvasLayer
 
 export var playing = false
-var config_exists = false
 var rng = RandomNumberGenerator.new()
-var cfg = null
-var ConfigDriver = preload("res://HevLib/pointers/ConfigDriver.gd")
+var pointers = ModLoader._savedObjects[0]
 func _ready():
 	visible = false
 	$AudioStreamPlayer.stop()
-	var dir = Directory.new()
-	if dir.file_exists("res://HevLib/pointers/ConfigDriver.gd"):
-		config_exists = true
-		cfg = load("res://HevLib/pointers/ConfigDriver.gd")
-	else:
-		config_exists = false
 
 func clear():
 	$Sprite.frame = 0
@@ -43,10 +35,9 @@ func start():
 
 func calculate_chance():
 	var chance = 10000
-	if config_exists:
-		var configuration = cfg.__get_config("1in10,000ChanceWitheredFoxyJumpscareEverySecond").get("FOXYJUMPSCARE_CFG",{})
-		chance = configuration.get("jumpscare_chance",10000)
-		interval = configuration.get("interval",1)
+	var configuration = pointers.ConfigDriver.__get_config("1in10,000ChanceWitheredFoxyJumpscareEverySecond").get("FOXYJUMPSCARE_CFG",{})
+	chance = configuration.get("jumpscare_chance",10000)
+	interval = configuration.get("interval",1)
 	var c = rng.randi_range(1,chance)
 	if c <= 1:
 		rng.randomize()
