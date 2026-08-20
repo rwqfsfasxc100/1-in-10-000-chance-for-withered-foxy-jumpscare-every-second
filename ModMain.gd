31c44c49835fd3463313af35dfb7c74eb408dac7
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-08-20 16:15:14 -0700

    Updated to 1.0.2

--- ModMain.gd
+++ ModMain.gd
@@ -1,3 +1,32 @@
+# [license]
+# 3-Clause BSD NON-AI License
+# 
+# Copyright 2026 __hev (Benjamin Buckhurst)
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+# 
+# 4. The source code and the binary form, and any modifications made to them may not be used for the purpose of input data, the training of, or improvement of machine learning algorithms,
+# including but not limited to artificial intelligence, natural language processing, or data mining. This condition applies to any derivatives,
+# modifications, or updates based on the Software code. Any usage of the source code or the binary form in an AI-training dataset is considered a breach of this License.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# [/license]
+
 extends Node
 
 # Set mod priority if you want it to load before/after other mods
@@ -7,7 +36,7 @@ const MOD_PRIORITY = 0
 const MOD_NAME = "1 in 10,000 Chance Withered Foxy Jumpscare Every Second"
 const MOD_VERSION_MAJOR = 1
 const MOD_VERSION_MINOR = 0
-const MOD_VERSION_BUGFIX = 1
+const MOD_VERSION_BUGFIX = 2
 const MOD_VERSION_METADATA = ""
 var modPath:String = get_script().resource_path.get_base_dir() + "/"
 
@@ -22,37 +51,53 @@ func _ready():
 	var CRoot = get_tree().get_root()
 	CRoot.call_deferred("add_child",layer)
 	l("Ready")
+
 func updateTL(path:String, delim:String = ",", useRelativePath:bool = true, fullLogging:bool = true):
 	if useRelativePath:
 		path = str(modPath + path)
 	l("Adding translations from: %s" % path)
 	var tlFile:File = File.new()
-	tlFile.open(path, File.READ)
-
+	var err = tlFile.open(path, File.READ)
+	
+	if err != OK:
+		return
+	
 	var translations := []
 	
 	var translationCount = 0
 	var csvLine := tlFile.get_line().split(delim)
+	
 	if fullLogging:
 		l("Adding translations as: %s" % csvLine)
 	for i in range(1, csvLine.size()):
 		var translationObject := Translation.new()
 		translationObject.locale = csvLine[i]
 		translations.append(translationObject)
-
+	
 	while not tlFile.eof_reached():
-		csvLine = tlFile.get_csv_line(delim)
-
-		if csvLine.size() > 1:
+		var line = tlFile.get_line()
+		if line.begins_with("#"):
+			continue
+		csvLine = line.split(delim)
+		var size = csvLine.size()
+		if size > 1:
+			if size > 2:
+				var i = 0
+				while i < size:
+					if csvLine[i].ends_with("\\") and i < size:
+						csvLine[i] = csvLine[i].rstrip("\\") + delim + csvLine[i + 1]
+						csvLine.remove(i + 1)
+						size -= 1
+					i += 1
 			var translationID := csvLine[0]
-			for i in range(1, csvLine.size()):
+			for i in range(1, size):
 				translations[i - 1].add_message(translationID, csvLine[i].c_unescape())
 			if fullLogging:
 				l("Added translation: %s" % csvLine)
 			translationCount += 1
-
+	
 	tlFile.close()
-
+	
 	for translationObject in translations:
 		TranslationServer.add_translation(translationObject)
 	l("%s Translations Updated" % translationCount)
